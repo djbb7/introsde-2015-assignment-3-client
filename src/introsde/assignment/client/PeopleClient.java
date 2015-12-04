4f642586ed1e7dcb0a28762ae40521ee80d9345f
djbb7 <danielbruzual@gmail.com>
2015-12-04 18:24:54 +0100

    Fix print format, add case number

--- src/introsde/assignment/client/PeopleClient.java
+++ src/introsde/assignment/client/PeopleClient.java
@@ -25,10 +25,11 @@ public class PeopleClient {
 
 	public static String testTitle = 
 			"\n############################################################################################################\n"+
-			"#  [Test %2d]    %s\n"+
-			"#  Methods used:  %s\n"+
-			"#\n";
-	public static int testCase = 0;
+			"#  [Test %2d]       %s\n"+
+			"#  Methods used:   %s\n"+
+			"#  Method numbers: %s\n"+
+			"#  Response: \n";
+	public static int testCase = 1;
 	
 	public static String personJoinTableHeader = 
 			" -----------------------------------------------------------------------------------------------------------\n"
@@ -42,21 +43,21 @@ public class PeopleClient {
 			" -------------------------------------------------------\n"
 			+String.format(" %5s%15s%15s%15s", "ID", "First Name", "Last Name", "Birthdate")
 			+"\n -------------------------------------------------------";
-	public static String personTableRow = "%5d%15s%15s%15s";
+	public static String personTableRow = " %5d%15s%15s%15s";
 
 	
 	public static String measureTypesTableHeader = 
 			" ----------------------------------\n"
 			+String.format(" %15s%15s", "Measure Type", "Value Type")
 			+"\n ----------------------------------";
-	public static String measureTypesTableRow = "%15s%15s";
+	public static String measureTypesTableRow = " %15s%15s";
 	
 	
 	public static String measureTableHeader = 
 			" ----------------------------------------------------------------\n"
 			+String.format(" %5s%20s%15s%20s", "mid", "Measure Type", "Value", "Date Registered")
 			+"\n ----------------------------------------------------------------";
-	public static String measureTableRow = "%5d%20s%15s%20s";
+	public static String measureTableRow = " %5d%20s%15s%20s";
 	
 	public static void main(String[] args){
 		People_Service peopleService = new People_Service();
@@ -69,29 +70,28 @@ public class PeopleClient {
 		/**********************************************************************
 		 * T0:	Get all people in database
 		 */
-		System.out.println(String.format(testTitle, testCase++, 
-				"Get all people in the database, along with their current health profile. Store the first.",
-				"readPersonList()"));
 		PeopleList peopleList = peoplePort.readPersonList();
 		List<Person> people = peopleList.getPeople();
+		System.out.println(String.format(testTitle, testCase++, 
+				"Get all people in the database, along with their current health profile. Store the first.",
+				"readPersonList()",
+				"#1"));
 		printPeopleWithMeasures(people);
 		
 		/**********************************************************************
 		 * T1: 	Read `first` person
 		 */
-		System.out.println(String.format(testTitle, testCase++,
-				"Read the `first` person again.",
-				"readPerson(Long id)"));
 		Person first = people.get(0);
 		Person repeatFirst = peoplePort.readPerson(first.getId());
+		System.out.println(String.format(testTitle, testCase++,
+				"Read the `first` person again.",
+				"readPerson("+people.get(0).getId()+")",
+				"#2"));
 		printPersonWithMeasures(repeatFirst);
 		
 		/**********************************************************************
 		 * T2:	Update `first` person's name and verify it was changed TODO: Check why date doesn't update
 		 */
-		System.out.println(String.format(testTitle, testCase++,
-				"Change the `first` person's name and birthdate and check it was updated.",
-				"updatePerson(Person p)"));
 		PersonUpdate pUpdate = new PersonUpdate();
 		pUpdate.setId(first.getId());
 		pUpdate.setBirthdate(first.getBirthdate());
@@ -99,6 +99,12 @@ public class PeopleClient {
 		pUpdate.setFirstname(new StringBuilder(first.getFirstname()).reverse().toString());
 		pUpdate.setLastname(first.getLastname());
 		Person responseP = peoplePort.updatePerson(pUpdate);
+		
+		System.out.println(String.format(testTitle, testCase++,
+				"Change the `first` person's name and birthdate and check it was updated.",
+				"updatePerson(Person<id:"+pUpdate.getId()+",firstname:"+pUpdate.getFirstname()
+				+", lastname:"+pUpdate.getLastname()+", birthdate:>"+pUpdate.getBirthdate()+")",
+				"#3"));
 		System.out.println("Before:");
 		printPerson(first);
 		System.out.println("After:");
@@ -107,31 +113,41 @@ public class PeopleClient {
 		/**********************************************************************
 		 * T3:   Read measureTypes
 		 */
-		System.out.println(String.format(testTitle, testCase++,
-				"Read all measure types.",
-				"readMeasureTypes()"));
 		MeasureTypes mTResponse = peoplePort.readMeasureTypes();
 		List<MeasureType> measureTypes = mTResponse.getMeasureTypes();
+		System.out.println(String.format(testTitle, testCase++,
+				"Read all measure types.",
+				"readMeasureTypes()",
+				"#7"));
 		printMeasureTypes(measureTypes);
 		
 		/**********************************************************************
 		 * T4:	Read measure history
 		 */
+		String methods = ""; 
+		for(int i=0; i<measureTypes.size(); i++){
+			methods += "readPersonHistory("+first.getId()+", "+measureTypes.get(i).getMeasureType()+"), ";
+		}
 		System.out.println(String.format(testTitle, testCase++,
-				"Read all measure history of some type for `first` person.",
-				"readPersonHistory(Long id, String measureType)"));
-		String targetMeasureType = measureTypes.get(0).getMeasureType();
-		HealthProfileHistory historyResponse = peoplePort.readPersonHistory(first.getId(), targetMeasureType);
-		List<Measure> history = historyResponse.getHealthProfileHistory();
-		printMeasures(history);
+				"Read measure history of all types for `first` person.",
+				methods.substring(0, methods.length()-2),
+				"#6"));
+		for(MeasureType mT: measureTypes){
+			String targetMeasureType = mT.getMeasureType();
+			HealthProfileHistory historyResponse = peoplePort.readPersonHistory(first.getId(), targetMeasureType);
+			List<Measure> history = historyResponse.getHealthProfileHistory();
+			printMeasures(history);
+		}
+		
 		
 		/**********************************************************************
-		 * T3:	Create person "Chuck Norris" with Health Profile TODO: ADD HEALTH PROFILE
+		 * T3:	Create person "Chuck Norris" with Health Profile.
 		 */
-		System.out.println(String.format(testTitle, testCase++,
-				"Create person `Chuck Norris` with Health Profile.",
-				"createPerson(Person p)"));
 		PersonCreate pCreate = new PersonCreate();
+		MeasureCreate mCreate = new MeasureCreate();
+		mCreate.setMeasureType(measureTypes.get(measureTypes.size()-1).getMeasureType());
+		mCreate.setMeasureValue("166");
+		pCreate.getCurrentHealth().add(mCreate);
 		pCreate.setFirstname("Chuck");
 		pCreate.setLastname("Norris");
 		GregorianCalendar c = new GregorianCalendar(1940, 3, 10);
@@ -142,41 +158,49 @@ public class PeopleClient {
 		} catch (DatatypeConfigurationException e) {
 		}
 		Person chuck = peoplePort.createPerson(pCreate);
+		System.out.println(String.format(testTitle, testCase++,
+				"Create person `Chuck Norris` with Health Profile.",
+				"createPerson(Person<firstname:Chuck, lastname:Norris, birthdate:03/10/1940, currentHealth: Measure<type: "
+				+mCreate.getMeasureType()+", value: "+mCreate.getMeasureValue()+"> >)",
+				"#4"));
 		printPersonWithMeasures(chuck);
 		
 		/**********************************************************************
 		 * T4:	Create a measure for "Chuck Norris"
 		 */
-		System.out.println(String.format(testTitle, testCase++,
-				"Add a new measure for the newly created person (`Chuck Norris`).",
-				"savePersonMeasure(Long id, Measure m)"));
-		MeasureCreate mCreate = new MeasureCreate();
-		mCreate.setMeasureType("weight");
+		mCreate = new MeasureCreate();
+		mCreate.setMeasureType(measureTypes.get(0).getMeasureType());
 		mCreate.setMeasureValue("68");
 		Measure chuckWeight = peoplePort.savePersonMeasure(chuck.getId(), mCreate);
+		System.out.println(String.format(testTitle, testCase++,
+				"Add a new measure for the newly created person (`Chuck Norris`).",
+				"savePersonMeasure("+chuck.getId()+", Measure<type: "+mCreate.getMeasureType()+", value: "+mCreate.getMeasureValue()+">)",
+				"#9"));
 		printMeasure(chuckWeight);
 		
 		/**********************************************************************
 		 * T5:	Update the measure's value
 		 */
-		System.out.println(String.format(testTitle, testCase++,
-				"Update the measure's value.",
-				"updatePersonMeasure(Long id, Measure m)"));
 		MeasureUpdate mUpdate = new MeasureUpdate();
-		mUpdate.setMeasureType("weight");
+		mUpdate.setMeasureType(measureTypes.get(0).getMeasureType());
 		mUpdate.setMeasureValue("78");
 		mUpdate.setMid(chuckWeight.getMid());
 		Measure updatedM = peoplePort.updatePersonMeasure(chuck.getId(), mUpdate);
+		System.out.println(String.format(testTitle, testCase++,
+				"Update the measure's value.",
+				"updatePersonMeasure("+chuck.getId()+", Measure<mid: "+mUpdate.getMid()+", type: "+mUpdate.getMeasureType()+", value: "+mUpdate.getMeasureValue()+">)",
+				"#10"));
 		printMeasure(updatedM);
 		
 		/**********************************************************************
-		 * T6:	Read measure to verify it was updated
+		 * T6:	Read measure to verify it was updated TODO: Not saving, check T5
 		 */
-		System.out.println(String.format(testTitle, testCase++,
-				"Re-read the measure to make sure it was updated.",
-				"readPersonMeasure(Long id, String measureType, Long mid)"));
 		Measure readUpdatedM = peoplePort.readPersonMeasure(chuck.getId(), 
 					updatedM.getMeasureType().getMeasureType(), updatedM.getMid());
+		System.out.println(String.format(testTitle, testCase++,
+				"Re-read the measure to make sure it was updated.",
+				"readPersonMeasure("+chuck.getId()+", "+updatedM.getMeasureType().getMeasureType()+", "+updatedM.getMid()+")",
+				"#8")); 
 		printMeasure(readUpdatedM);
 		
 		/**********************************************************************
@@ -184,11 +208,17 @@ public class PeopleClient {
 		 */
 		System.out.println(String.format(testTitle, testCase++,
 				"Delete `Chuck Norris`",
-				"deletePerson(Long id), readPerson(Long id) to confirm it was deleted"));
-		peoplePort.deletePerson(chuck.getId());
+				"deletePerson("+chuck.getId()+"), readPerson("+chuck.getId()+") to confirm it was deleted",
+				"#5"));
+		System.out.println("Deleting person...");
+		boolean res = peoplePort.deletePerson(chuck.getId());
+		if(res){
+			System.out.println("\tPerson deleted successfully");
+		}
+		System.out.println("\nDouble-checking that person is not in db...");
 		Person checkDelete = peoplePort.readPerson(chuck.getId());
 		if(checkDelete == null){
-			System.out.println("Person with id `"+chuck.getId()+"` could not be found.");
+			System.out.println("\tPerson with id `"+chuck.getId()+"` could not be found.");
 		}
 	}
 	
@@ -236,7 +266,7 @@ public class PeopleClient {
 			System.out.println(String.format(personJoinEmptyMeasureRow, id, firstname, lastname, dateString, "", "", "", ""));
 		} else {
 			for(Measure m : p.getCurrentHealth()){
-				String mDate = mFormatter.format(p.getBirthdate().toGregorianCalendar().getTime());
+				String mDate = mFormatter.format(m.getDateRegistered().toGregorianCalendar().getTime());
 				System.out.println(String.format(personJoinRow, id, firstname, lastname, dateString, m.getMeasureType().getMeasureType(), m.getMeasureValue(), mDate, m.getMid()));
 			}
 		}
